dd970c68f858f5f106ede4a0eab5a8e708e2c190
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2021-08-28 19:12:47 -0500

    coso

--- co/edu/unbosque/model/Model.py
+++ co/edu/unbosque/model/Model.py
@@ -19,3 +19,41 @@ class Sort:
         for i in range(quantity):
             randomList.append(random.randint(0, quantity))
         return randomList
+
+    def doSelectionSort(self, array):
+        for i in range(len(array)):
+            minim = i
+            for j in range(i, len(array)):
+                if array[j] < array[minim]:
+                    minim = j
+            if minim != i:
+                orderedNum = array[i]
+                array[i] = array[minim]
+                array[minim] = orderedNum
+
+        return array
+
+    def doRadixSort(self, array):
+        for i in range(len(array)):
+            array
+
+    def doQuickSort(self, array):
+        left = []
+        center = []
+        right = []
+        if len(array) > 1:
+            pivot = array[0]
+            for i in array:
+                if i < pivot:
+                    left.append(i)
+                elif i == pivot:
+                    center.append(i)
+                elif i > pivot:
+                    right.append(i)
+            return self.doQuickSorto(left) + center + self.doQuickSorto(right)
+        else:
+            return array
+
+    def doMergeSort(self, array):
+        for i in range(len(array)):
+            array
